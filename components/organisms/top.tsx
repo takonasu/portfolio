b0d7bbab7178cf897745dbfd64915d0c111b7f82
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-06-16 22:48:07 +0900

    add: PC版TOP

--- components/organisms/top.tsx
+++ components/organisms/top.tsx
@@ -5,7 +5,28 @@ type Props = {};
 export const Top: React.FC<Props> = ({}) => {
 	return (
 		<div className={styles.main}>
-			<h1>Takonasu&apos;s Portfolio</h1>
+			<div className={styles.top}>
+				<div className={styles.topMenuFrame}>
+					<div className={styles.titleFrame}>
+						<h1>Takonasu&apos;s Portfolio</h1>
+						<p>A Master&apos;s Student Of Computer Science At Univ. Tsukuba.</p>
+					</div>
+					<div className={styles.linksFrame}>
+						<div>
+							<a href="">About</a>
+						</div>
+						<div>
+							<a href="">Works</a>
+						</div>
+						<div>
+							<a href="">Links</a>
+						</div>
+						<div>
+							<a href="">Contact</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
