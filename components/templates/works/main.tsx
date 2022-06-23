75b643f017f0a747f406188703d3a78beeebac5f
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-06-23 23:20:10 +0900

    add: tag category endpoint

--- components/templates/works/main.tsx
+++ components/templates/works/main.tsx
@@ -1,13 +1,18 @@
 import styles from '../../../styles/components/templates/works/Main.module.scss';
 import ArticleFrame from '../../organisms/articleFrame';
 import Card from '../../molecules/card';
-import { work } from '../../../types/cms-types';
+import { work, categories, tags } from '../../../types/cms-types';
 
 type Props = {
 	works: [work];
+	categories: [categories];
+	tags: [tags];
 };
 
-export const Main: React.FC<Props> = ({ works }) => {
+export const Main: React.FC<Props> = ({ works, categories, tags }) => {
+	// console.log(works);
+	// console.log(categories);
+	// console.log(tags);
 	return (
 		<ArticleFrame>
 			<div className={styles.workCards}>
@@ -17,6 +22,32 @@ export const Main: React.FC<Props> = ({ works }) => {
 							key={elm.id}
 							image="/images/twinte.jpg"
 							title={elm.title}
+							detail={elm.tags.toString()}
+							link={`/works/${elm.id}`}
+						/>
+					);
+				})}
+			</div>
+			<div className={styles.workCards}>
+				{categories.map((elm) => {
+					return (
+						<Card
+							key={elm.id}
+							image="/images/twinte.jpg"
+							title={elm.name}
+							detail="{elm.body}"
+							link={`/works/${elm.id}`}
+						/>
+					);
+				})}
+			</div>
+			<div className={styles.workCards}>
+				{tags.map((elm) => {
+					return (
+						<Card
+							key={elm.id}
+							image="/images/twinte.jpg"
+							title={elm.name}
 							detail="{elm.body}"
 							link={`/works/${elm.id}`}
 						/>
--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,26 +1,35 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { client } from '../../libs/client';
-import { work } from '../../types/cms-types';
+import { work, categories, tags } from '../../types/cms-types';
 import Main from '../../components/templates/works/main';
 
 type Props = {
 	works: [work];
+	categories: [categories];
+	tags: [tags];
 };
 
-const Home = ({ works }: Props) => {
+const Home = ({ works, categories, tags }: Props) => {
 	return (
 		<div>
-			<Main works={works} />
+			<Main works={works} categories={categories} tags={tags} />
 		</div>
 	);
 };
 
 export const getStaticProps = async () => {
-	const data = await client.get({ endpoint: 'work' });
+	const apiResult = await Promise.all([
+		client.get({ endpoint: 'work' }),
+		client.get({ endpoint: 'categories' }),
+		client.get({ endpoint: 'tags' })
+	]);
+
 	return {
 		props: {
-			works: data.contents
+			works: apiResult[0].contents,
+			categories: apiResult[1].contents,
+			tags: apiResult[2].contents
 		}
 	};
 };
