54452ea0d098ec72e6b591cd1824864748e29967
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-06-27 17:17:08 +0900

    add: work一覧スタイル

--- components/templates/works/main.tsx
+++ components/templates/works/main.tsx
@@ -13,37 +13,35 @@ type Props = {
 export const Main: React.FC<Props> = ({ works, categories, tags }) => {
 	return (
 		<ArticleFrame>
+			<h1>All Works</h1>
 			<div className={styles.workCards}>
 				{works.map((elm) => {
 					return (
-						<Card
-							key={elm.id}
-							image="/images/twinte.jpg"
-							title={elm.title}
-							detail={elm.tags.toString()}
-							link={`/works/${elm.id}`}
-						/>
+						<div key={elm.id} className={styles.card}>
+							<Card
+								image="/images/twinte.jpg"
+								title={elm.title}
+								detail={elm.tags.toString()}
+								link={`/works/${elm.id}`}
+							/>
+						</div>
 					);
 				})}
 			</div>
 			<div className={styles.workCards}>
 				{categories.map((elm) => {
 					return (
-						<Card
-							key={elm.id}
-							image="/images/twinte.jpg"
-							title={elm.name}
-							detail="{elm.body}"
-							link={`/works/${elm.id}`}
-						/>
+						<div key={elm.id} className={styles.card}>
+							<Card image="/images/twinte.jpg" title={elm.name} detail="{elm.body}" link={`/works/${elm.id}`} />
+						</div>
 					);
 				})}
 			</div>
-			<div className={styles.workCards}>
+			{/* <div className={styles.workCards}>
 				{tags.map((elm) => {
 					return <Tag key={elm.id} label={elm.name} />;
 				})}
-			</div>
+			</div> */}
 		</ArticleFrame>
 	);
 };
