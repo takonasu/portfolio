bd927720615187b3f52467344f48e89898422af0
takonasu <piano88_viola_tako8@hotmail.co.jp>
2023-08-22 15:04:12 +0900

    add: 社会人スターターキット

--- components/organisms/top/topAbout.tsx
+++ components/organisms/top/topAbout.tsx
@@ -42,6 +42,8 @@ export const TopAbout: React.FC<Props> = ({}) => {
 			</MainArticle>
 			<MainArticle title="免許・資格">
 				<NormalList day="2023年8月" title="第一級アマチュア無線技士 免許取得" />
+				<NormalList day="2023年7月" title="FP3級 試験合格" />
+				<NormalList day="2023年2月" title="日商簿記3級 試験合格" />
 				<NormalList day="2022年6月" title="情報処理安全確保支援士試験 試験合格" />
 				<NormalList day="2021年6月" title="ネットワークスペシャリスト 試験合格" />
 				<NormalList day="2021年1月" title="第二種電気工事士 免許取得" />
